ddf320157e568dde57ba5457cba2f060c386b05b
Andrei Polyakov <ap120368@gmail.com>
2021-11-06 12:16:42 +0500

    Minor changes

--- src/axios-common.js
+++ src/axios-common.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const AXIOS = axios.create({
   //baseURL: 'http://localhost:8000',
-  baseURL: 'http://abp-oci2.tk/api',
+  baseURL: 'https://abp-oci2.tk/api',
   withCredentials: true
 })
