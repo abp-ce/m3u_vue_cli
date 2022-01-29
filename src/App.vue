407130c0b296d56f53e3d8a53a7c055bbf0fbe4c
Andrei Polyakov <ap120368@gmail.com>
2022-01-29 14:54:07 +0500

    Button icons added

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <b-container>
     <b-navbar>
       <!--b-button v-b-toggle.sample class="float-left">Sample</b-button-->
-      <b-nav><b-nav-item v-b-toggle.sample class="float-left">Sample List</b-nav-item></b-nav>
+      <b-nav><b-nav-item v-b-toggle.sample class="float-left" v-b-tooltip.hover title="Sample List">
+        <b-icon-file></b-icon-file>
+      </b-nav-item></b-nav>
       <b-navbar-brand to="/">M3U editor</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-nav-item v-if = "this.$store.getters.isLoggedIn" id="nav-user">
           <b-icon-person-check></b-icon-person-check>
         </b-nav-item>
         <b-tooltip target="nav-user">{{this.$store.state.user}}</b-tooltip>
-        <b-nav-item v-if = "this.$store.getters.isLoggedIn"  v-b-tooltip.hover title="Logout" @click="logout">
+        <b-nav-item v-if = "this.$store.getters.isLoggedIn" v-b-tooltip.hover title="Logout" @click="logout">
           <b-icon-person-dash></b-icon-person-dash>
         </b-nav-item>
         <b-nav-item v-if = "!this.$store.getters.isLoggedIn" to="/login/reg" v-b-tooltip.hover title="Register">
@@ -19,7 +21,9 @@
           <b-icon-person></b-icon-person>
         </b-nav-item>
       </b-navbar-nav>
-      <b-nav><b-nav-item v-b-toggle.personal class="float-left">Personal List</b-nav-item></b-nav>
+      <b-nav><b-nav-item v-b-toggle.personal class="float-left" v-b-tooltip.hover title="Personal List">
+        <b-icon-file-person></b-icon-file-person>
+      </b-nav-item></b-nav>
       <!--b-button v-b-toggle.personal class="float-right">Personal</b-button-->
     </b-navbar>
     <router-view/>
